798064fdd175bc28000ac362a321cef91d1a42e5
Arkadii-Bogdanov <innakokina@air-inna.localdomain>
2022-10-29 23:05:02 +0300

    initial commit

---
+++ BitOperations.java
@@ -0,0 +1,57 @@
+
+
+
+// if wrong wrong  -1
+// if right number 
+
+/**
+ * 
+ * @param number - any number 
+ *@param nBit- number of bit
+ *@return  - value of bit with number nBit
+ */
+ 
+
+public class BitOperations {
+	
+
+
+	static public int getBitValue(long number, int nBit) {
+		return -1;
+	}
+	
+	
+	
+	
+	
+	
+//	va
+//number any  
+//nBit - bit number
+//value -true 1 or 0
+//return number in which value of nBit bit will a given value
+	
+	static public long setBitValue(long number, int nBit, int value) {
+		
+		return -1;
+	}
+//d 
+	
+	
+
+//number -  any
+//nBit - bit number
+//value -true 1 or 0
+//return number in which value of nBit bit will be replaced(old value)
+	
+	
+	static public long revertValue(long number, int nBit) {
+		
+		return -1;
+	}
+	
+	
+	
+	
+	
+}
